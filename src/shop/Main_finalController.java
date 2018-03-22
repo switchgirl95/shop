fb594d47c8b9b59a41d602bae958d32c5e21cec9
Andin Fokunang <a.fokunang@gmail.com>
2018-03-22 20:59:16 +0100

    mettre une partie en commentaires...

--- src/shop/Main_finalController.java
+++ src/shop/Main_finalController.java
@@ -143,14 +143,14 @@ public class Main_finalController implements Initializable {
         initTableProd();
         addCategory.setVisible(false);
         prepareSlideMenuAnimation();
-        fillTableProd();
+       /* fillTableProd();
         try {
             root = FXMLLoader.load(getClass().getResource("prodPhoto.fxml"));
         } catch (IOException ex) {
             Logger.getLogger(Main_finalController.class.getName()).log(Level.SEVERE, null, ex);
         }
         photoList.getChildren().add(root);
-        
+        */
     }
 
     
